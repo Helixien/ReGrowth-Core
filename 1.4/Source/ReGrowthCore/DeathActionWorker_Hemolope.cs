de11fc3633342c5a605a4fd7bf17dc641be6f9c3
Taranchuk <nfhfyxer@gmail.com>
2023-05-30 19:43:39 +0300

    pollution range + hemolope death worker

---
+++ 1.4/Source/ReGrowthCore/DeathActionWorker_Hemolope.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ReGrowthCore
+{
+    public class DeathActionWorker_Hemolope : DeathActionWorker
+    {
+        public override void PawnDied(Corpse corpse)
+        {
+            FilthMaker.TryMakeFilth(corpse.PositionHeld, corpse.MapHeld, ThingDefOf.Filth_Blood, count: Rand.RangeInclusive(7, 10), shouldPropagate: true);
+        }
+    }
+}

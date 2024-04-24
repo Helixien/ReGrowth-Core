df3785accc4996c74e9bfbe5d0a34e1fa8dea798
Taranchuk <nfhfyxer@gmail.com>
2024-04-24 20:45:15 +0300

    biome kid patch fixes

--- 1.5/Source/ReGrowthCore/BiomesKit/Patches.cs
+++ 1.5/Source/ReGrowthCore/BiomesKit/Patches.cs
@@ -17,13 +17,27 @@ namespace ReGrowthCore
     {
         public static void Postfix(XmlDocument xmlDoc, Dictionary<XmlNode, LoadableXmlAsset> assetlookup)
         {
-            var patchOp = new PatchOperationAttributeSet
+            var patchOpSwapExtension = new PatchOperationAttributeSet
             {
                 xpath = "Defs/BiomeDef/modExtensions/li[@Class=\"BiomesKit.BiomesKitControls\"]",
                 attribute = "Class",
-                value = "RegrowthCore.BiomesKitControl"
+                value = "RegrowthCore.BiomesKitControl",
+                success = PatchOperation.Success.Always,
             };
-            patchOp.Apply(xmlDoc);
+            patchOpSwapExtension.Apply(xmlDoc);
+            string xmlText = "<workerClass>ReGrowthCore.UniversalBiomeWorker</workerClass>";
+            XmlDocument xmlDoc2 = new XmlDocument();
+            xmlDoc2.LoadXml(xmlText);
+            var patchOpSwapBiomeWorker = new PatchOperationReplace
+            {
+                xpath = "Defs/BiomeDef[workerClass=\"BiomesKit.UniversalBiomeWorker\"]/workerClass",
+                value = new XmlContainer
+                {
+                    node = xmlDoc2
+                },
+                success = PatchOperation.Success.Always,
+            };
+            patchOpSwapBiomeWorker.Apply(xmlDoc);
         }
     }
 

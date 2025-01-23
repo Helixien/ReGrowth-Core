6da234b49ca83dd1eba499ba6cf744868c4c0086
Taranchuk <nfhfyxer@gmail.com>
2025-01-23 18:22:32 +0200

    crash fix

--- 1.5/Source/ReGrowthCore/PerspectiveOres/Map_FinalizeInit_Patch.cs
+++ 1.5/Source/ReGrowthCore/PerspectiveOres/Map_FinalizeInit_Patch.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using RimWorld;
 using UnityEngine;
 using static ReGrowthCore.PerspectiveOresUtility;
+
 namespace ReGrowthCore
-{   
+{
     [HarmonyPatch(typeof(Map), nameof(Map.FinalizeInit))]
-	public static class Map_FinalizeInit_Patch
+    public static class Map_FinalizeInit_Patch
     {
         public static void Postfix(Map __instance)
         {
@@ -18,14 +19,14 @@ namespace ReGrowthCore
         {
             Dictionary<(GraphicData, Color), Graphic> graphicCache = new Dictionary<(GraphicData, Color), Graphic>();
             Dictionary<Building, int> lumps = new Dictionary<Building, int>(); //thingID, lumpID
-            Dictionary<int, Color> lumpColors = new Dictionary<int, Color>(); //lumpID, assosicated stone
+            Dictionary<int, Color> lumpColors = new Dictionary<int, Color>(); //lumpID, associated stone
             int nextLumpID = 0;
-            
+
             var list = map.listerThings.listsByGroup[2];
             for (int i = list.Count; i-- > 0;)
             {
                 var thing = list[i];
-                if (thing is not Mineable mineable || ModSettings_PerspectiveOres.skippedMineableDefs.Contains(thing.def.defName)) 
+                if (thing is not Mineable mineable || ModSettings_PerspectiveOres.skippedMineableDefs.Contains(thing.def.defName))
                 {
                     if (reset) thing.graphicInt = null; //Reset
                     continue;
@@ -38,6 +39,7 @@ namespace ReGrowthCore
                     DetermineLump(thing.def, nextLumpID++, thing.Position);
                 }
             }
+
             if (lumps.Count == 0)
             {
                 if (reset) map.mapDrawer.RegenerateEverythingNow();
@@ -47,31 +49,40 @@ namespace ReGrowthCore
 
             AssociateLumps();
             LongEventHandler.QueueLongEvent(() => RecolorMineables(), null, false, null);
-            LongEventHandler.QueueLongEvent(() => map.mapDrawer.RegenerateEverythingNow(), null, false, null);
+            LongEventHandler.QueueLongEvent(() => map.mapDrawer.RegenerateEverythingNow(), null, false, null); // Only regenerate once
 
-            void DetermineLump(ThingDef def, int lumpID, IntVec3 pos)
+            void DetermineLump(ThingDef def, int lumpID, IntVec3 startPos)
             {
-                foreach (IntVec3 item in GenAdjFast.AdjacentCells8Way(pos).ToArray())
-                {
-                    try
-                    {
-                        if (item.InBounds(map))
-                        {
-                            var edifice = item.GetEdifice(map);
-                            if (edifice == null || edifice.def != def || lumps.ContainsKey(edifice) 
-                                || edifice is not Mineable mineableEdifice 
-                                || ModSettings_PerspectiveOres.skippedMineableDefs.Contains(mineableEdifice.def.defName)) continue;
-                            if (!mineableEdifice.def.building.isResourceRock) continue;
-                            lumps.Add(edifice, lumpID);
-                            DetermineLump(def, lumpID, item);
-                        }
-                    }
-                    catch
-                    {
+                Queue<IntVec3> cellsToProcess = new Queue<IntVec3>();
+                cellsToProcess.Enqueue(startPos);
+                HashSet<IntVec3> processedCells = new HashSet<IntVec3>(); // Track processed cells to avoid redundant checks
 
+                while (cellsToProcess.Count > 0)
+                {
+                    IntVec3 pos = cellsToProcess.Dequeue();
+                    if (!processedCells.Add(pos)) continue; // Skip if already processed
+
+                    foreach (IntVec3 item in GenAdjFast.AdjacentCells8Way(pos).ToArray())
+                    {
+                        if (!item.InBounds(map)) continue;
+
+                        var edifice = item.GetEdifice(map);
+                        if (edifice == null || edifice.def != def || lumps.ContainsKey(edifice)
+                            || edifice is not Mineable mineableEdifice
+                            || ModSettings_PerspectiveOres.skippedMineableDefs.Contains(mineableEdifice.def.defName))
+                        {
+                            continue;
+                        }
+                        if (!mineableEdifice.def.building.isResourceRock) continue;
+                        if (!lumps.ContainsKey(edifice))
+                        {
+                            lumps.Add(edifice, lumpID);
+                            cellsToProcess.Enqueue(item);
+                        }
                     }
                 }
             }
+
             void AssociateLumps()
             {
                 foreach (var (mineable, lumpID) in lumps) //Key = the mineable thing, Value = its lumpID
@@ -81,22 +92,18 @@ namespace ReGrowthCore
                     //Look around this mineral and try to find a stone
                     foreach (var pos in GenAdjFast.AdjacentCells8Way(mineable.Position))
                     {
-                        try
-                        {
-                            if (pos.InBounds(map))
-                            {
-                                var edifice = pos.GetEdifice(map);
-                                if (edifice == null || edifice.def == mineable.def || edifice is not Mineable mineableEdifice) continue;
-                                if (mineableEdifice.def.building.isResourceRock || !mineableEdifice.def.building.isNaturalRock) continue;
+                        if (!pos.InBounds(map)) continue;
 
-                                lumpColors.Add(lumpID, edifice.DrawColor);
-                                break;
-                            }
-                        }
-                        catch { }
+                        var edifice = pos.GetEdifice(map);
+                        if (edifice == null || edifice.def == mineable.def || edifice is not Mineable mineableEdifice) continue;
+                        if (mineableEdifice.def.building.isResourceRock || !mineableEdifice.def.building.isNaturalRock) continue;
+
+                        lumpColors.Add(lumpID, edifice.DrawColor);
+                        break; // Found a color, no need to check other neighbors for this lump
                     }
                 }
             }
+
             void RecolorMineables()
             {
                 foreach (var (mineable, lumpID) in lumps)
@@ -107,9 +114,9 @@ namespace ReGrowthCore
                     //Check if the graphic already exists
                     if (!graphicCache.TryGetValue((graphic, color), out Graphic cachedGraphic))
                     {
-                        cachedGraphic = GraphicDatabase.Get(graphic.graphicClass, graphic.texPath, 
+                        cachedGraphic = GraphicDatabase.Get(graphic.graphicClass, graphic.texPath,
                             (graphic.shaderType ?? ShaderTypeDefOf.Cutout).Shader, graphic.drawSize, color, graphic.colorTwo, graphic, graphic.shaderParameters, graphic.maskPath);
-                        
+
                         cachedGraphic = GraphicUtility.WrapLinked(cachedGraphic, graphic.linkType);
                         graphicCache.Add((graphic, color), cachedGraphic);
                     }
